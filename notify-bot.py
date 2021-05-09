172adf90f1defde37d6e59686b9604e6df4a4958
sxeix <sxeixarts@gmail.com>
2021-05-09 19:32:36 +0100

    Reply to a message when a keyword is mentioned

--- notify-bot.py
+++ notify-bot.py
@@ -1,8 +1,8 @@
 import discord
-import re
 from discord.ext import commands
 
 client = commands.Bot(command_prefix= '!')
+keywords = dict()
 
 @client.event
 async def on_ready():
@@ -10,15 +10,35 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!notify'):
-        print("command is used")
-    elif "test" in message.content:
-        print("contains the word test")
-    
+    if message.author == client.user:
+        return
+    for user in keywords:
+        if keywords[user] in message.content:
+            userId ="<@{id}>".format(id=user.id)
+            await message.reply(
+                "'{keyword}', was mentioned {user}"
+                .format(
+                    keyword = keywords[user], 
+                    user = userId
+                    )
+                )
+            print(user, keywords[user])
+    await client.process_commands(message)
+
+@client.command(description="Enter a keyword or keyphrase you would like to be notifed about")
+async def notify(ctx, arg):
+    keywords[ctx.author] = arg
+    print(keywords)
+    await ctx.send(
+        "I have stored your keyphrase, '{keyphrase}'"
+        .format(
+            keyphrase = arg
+            )
+        )
+
 def fetchToken():
     with open('token') as file: 
         token = file.readline()
     return token
-
 
 client.run(fetchToken())
