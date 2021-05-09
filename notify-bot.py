5013403a252433b4e8d72472301ff908257074b5
sxeix <sxeixarts@gmail.com>
2021-05-09 19:46:44 +0100

    Allow multiple keywords

--- notify-bot.py
+++ notify-bot.py
@@ -13,21 +13,24 @@ async def on_message(message):
     if message.author == client.user:
         return
     for user in keywords:
-        if keywords[user] in message.content:
-            userId ="<@{id}>".format(id=user.id)
-            await message.reply(
-                "'{keyword}', was mentioned {user}"
-                .format(
-                    keyword = keywords[user], 
-                    user = userId
+        for keyword in keywords[user]:
+            if keyword in message.content:
+                userId ="<@{id}>".format(id=user.id)
+                await message.reply(
+                    "'{keyword}', was mentioned {user}"
+                    .format(
+                        keyword = keyword, 
+                        user = userId
+                        )
                     )
-                )
-            print(user, keywords[user])
+                print(user, keywords[user])
     await client.process_commands(message)
 
 @client.command(description="Enter a keyword or keyphrase you would like to be notifed about")
 async def notify(ctx, arg):
-    keywords[ctx.author] = arg
+    if ctx.author not in keywords:
+        keywords[ctx.author] =[]
+    keywords[ctx.author].append(arg)
     print(keywords)
     await ctx.send(
         "I have stored your keyphrase, '{keyphrase}'"
