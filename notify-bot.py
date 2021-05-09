4db4b7d3f696e554f6274c7340e5e107496d8723
sxeix <sxeixarts@gmail.com>
2021-05-09 20:17:35 +0100

    Support multiple servers

--- notify-bot.py
+++ notify-bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix= '!')
-keywords = dict()
+servers = dict()
 
 @client.event
 async def on_ready():
@@ -12,26 +12,28 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    for user in keywords:
-        for keyword in keywords[user]:
-            if keyword in message.content:
-                userId ="<@{id}>".format(id=user.id)
-                await message.reply(
-                    "'{keyword}', was mentioned {user}"
-                    .format(
-                        keyword = keyword, 
-                        user = userId
+    if message.guild.id in servers:
+        for user in servers[message.guild.id]:
+            for keyword in servers[message.guild.id][user]:
+                if keyword in message.content.lower():
+                    userId ="<@{id}>".format(id=user.id)
+                    await message.reply(
+                        "'{keyword}', was mentioned {user}"
+                        .format(
+                            keyword = keyword, 
+                            user = userId
+                            )
                         )
-                    )
-                print(user, keywords[user])
+                    print(user, servers[message.guild.id][user])
     await client.process_commands(message)
 
-@client.command(description="Enter a keyword or keyphrase you would like to be notifed about")
+@client.command(description="Enter a keyword or keyphrase you would like to be notifed about use quotations for phrases longer than one word")
 async def notify(ctx, arg):
-    if ctx.author not in keywords:
-        keywords[ctx.author] =[]
-    keywords[ctx.author].append(arg)
-    print(keywords)
+    if ctx.guild.id not in servers:
+        servers[ctx.guild.id] = dict()
+    if ctx.author not in servers[ctx.guild.id]:
+        servers[ctx.guild.id][ctx.author] =[]
+    servers[ctx.guild.id][ctx.author].append(arg.lower())
     await ctx.send(
         "I have stored your keyphrase, '{keyphrase}'"
         .format(
